9af5ce3d51265416c25d7edc224d90c1c0baad57
doremonRudraksh <90757183+doremonRudraksh@users.noreply.github.com>
2022-01-19 19:29:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("my_picture.png");
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x: "+results[0].pose.nose.x);
+        console.log("nose y: "+results[0].pose.nose.y);
+    };
+}
